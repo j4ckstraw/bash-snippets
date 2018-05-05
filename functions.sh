1c0edfa45dd4bda8a4a00afa5bf9cbc3caefd1ff
jackstraw <1107247946@qq.com>
2018-05-05 16:28:41 +0800

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -3,6 +3,9 @@
 # history: 2018/05/05
 # function: collect bash snippets, some function you may needed.
 
+# DEBUG switch
+# set -x
+
 #############
 #RETURE CODE#
 #############
@@ -32,5 +35,35 @@ function root_privilege_check() {
     fi
 }
 
-root_privilege_check
-usage
+function menu() {
+	# http://programmingexamples.wikidot.com/bash-scripting
+    while :
+    do
+        clear
+        echo "-------------------------------------"
+        echo " Main Menu "
+        echo "-------------------------------------"
+        echo "[1] Show Todays date/time"
+        echo "[2] Show files in current directory"
+        echo "[3] Show calendar"
+        echo "[4] Start editor to write letters"
+        echo "[5] Exit/Stop"
+        echo "======================="
+        echo -n "Enter your menu choice [1-5]: "
+        read your_choice
+        case $your_choice in
+            1) echo "Today is `date` , press a key. . ." ; read ;;
+            2) echo "Files in `pwd`" ; ls -l ; echo "Press a key. . ." ; read ;;
+            3) cal ; echo "Press a key. . ." ; read ;;
+            4) vi ;;
+            5) exit $SUCCESS ;;
+            *) echo "Opps!!! Please select choice 1,2,3,4, or 5";
+               echo "Press a key. . ." ; read ;;
+         esac
+    done
+}
+
+# root_privilege_check
+# usage
+menu
+exit $SUCCESS
